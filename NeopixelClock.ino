b26ff2fb1d240ec702c8b4602ec314b8ed53784e
grgodkin <grgodkin@yahoo.com>
2021-07-07 20:35:32 -0500

    Simple MQTT pub/sub

--- NeopixelClock.ino
+++ NeopixelClock.ino
@@ -23,6 +23,8 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
+#include <PubSubClient.h>
+
 #if !defined(ESP32) && !defined(ESP8266)
   #error This sketch works only for the ESP8266 and ESP32
 #endif
@@ -51,6 +53,11 @@ acetime_t prevSeconds = 0;
 static const char SSID[] = SSID1;
 static const char PASSWORD[] = PWD1;
 
+const char* mqtt_server = mqttServer1;
+const int mqttPort = mqttPort1;
+const char* mqttUser = mqttUser1;
+const char* mqttPassword = mqttPassword1;
+
 static BasicZoneProcessor chicagoProcessor;
 static NtpClock ntpClock;
 static SystemClockLoop systemClock(&ntpClock, nullptr /*backup*/);
@@ -66,7 +73,57 @@ const uint16_t colors[] = {
   matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255)
 };
 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    //digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    //digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqttUser1, mqttPassword1)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void setup() {
+
+  Serial.begin(115200);
+  
   matrix.begin();
   matrix.setTextWrap(false);
   matrix.setBrightness(40);
@@ -114,10 +171,18 @@ void setup() {
   }
   
   ArduinoOTA.begin();
+  
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
 }
 
 void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
   if (millis() < 300000) {
       ArduinoOTA.handle();
