d314452b0d43533b23bf2a8412b00f97fb75d916
Greg Godkin <grgodkin@yahoo.com>
2026-09-11 10:21:56 -0600

    Add display network status indicators

--- include/display.h
+++ include/display.h
@@ -9,6 +9,7 @@ public:
     void showTime(int hours, int minutes);
     void updateColon(bool on);
     void showGarageClosed(bool closed);
+    void showNetworkStatus(bool wifiConnected, bool ntpSynced);
     void showMessage(const char* message);
 
     void clear();
